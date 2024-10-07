f57076b658fc3847e5f0432a122657cda637ed5f
Kyanvdanker <kyanvdanker@gmail.com>
2024-10-07 12:19:57 +0200

    fifth

---
+++ Coding/Python/Arduino-python.py
@@ -0,0 +1,10 @@
+import time
+import serial
+
+ser = serial.Serial(
+    port='',
+    baudrate=9600,
+    parity=serial.PARITY_ODD,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.SEVENBITS
+)
